18b8a4f4c1ed2d640926f8befb175b7ec38edb0c
Chris Uga <mechachristopher@gmail.com>
2022-05-28 14:39:14 -0400

    Update rps.js to end the game when a score reaches 5.

--- rps.js
+++ rps.js
@@ -1,8 +1,12 @@
-function computerPlay() {
-  let computersHand = Math.floor(Math.random()*3);
-  if (computersHand == 0) {
+let humanScore = 0;
+let robotScore = 0;
+let currentlyPlaying = true;
+
+function robotPlay() {
+  let robotsHand = Math.floor(Math.random()*3);
+  if (robotsHand == 0) {
     return "Rock";
-  } else if (computersHand == 1) {
+  } else if (robotsHand == 1) {
     return "Paper";
   } else {
     return "Scissors";
@@ -22,35 +26,166 @@ function humanPlay() {
   }
 }
 
-function compareHands(humanSelection, computerSelection) {
-  computersHand = computerPlay();
+function compareHands() {
+  robotsHand = robotPlay();
   humanHand = humanPlay();
-  if (computersHand == 'Rock') {
+  if (robotsHand == 'Rock') {
     if (humanHand == 'Rock') {
-      return 'Tie';
+      return keepScore('Tie');
     } else if (humanHand == 'Paper') {
-      return 'You win!';
+      return keepScore('You win!');
     } else {
-      return 'You lose... that\'s kinda cringe';
+      return keepScore('You lose... that\'s kinda cringe');
     }
-  } else if (computersHand == 'Paper') {
+  } else if (robotsHand == 'Paper') {
     if (humanHand == 'Rock') {
-      return 'You lose... that\'s kinda cringe';
+      return keepScore('You lose... that\'s kinda cringe');
     } else if (humanHand == 'Paper') {
-      return 'Tie';
+      return keepScore('Tie');
     } else {
-      return 'You win!';
+      return keepScore('You win!');
     }
   } else {
     if (humanHand == 'Rock') {
-      return 'You win!';
+      return keepScore('You win!');
     } else if (humanHand == 'Paper') {
-      return 'You lose... that\'s kinda cringe';
+      return keepScore('You lose... that\'s kinda cringe');
     } else {
-      return 'Tie';
+      return keepScore('Tie');
     }
   }
 }
 
-result = compareHands();
-console.log(result);
+function keepScore(string) {
+  if (string == 'You win!') {
+    humanScore++;
+    if (humanScore >= 5){
+      currentlyPlaying = false;
+      return 'Supreme victory!' + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+    }
+    return string + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+  } else if (string == 'You lose... that\'s kinda cringe') {
+    robotScore++;
+    if (robotScore >= 5){
+      currentlyPlaying = false;
+      return 'Mortifying failure!' + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+    }
+    return string + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+  } else if (string == 'Tie') {
+    return string + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+  }
+}
+
+function playGame() {
+  currentlyPlaying = true;
+  humanScore = 0;
+  robotScore = 0;
+  do {
+    console.log(compareHands());
+  } while (currentlyPlaying)
+}
+
+playGame()
+
+/*
+
+let currentlyPlaying = true;
+let humanScore = 0;
+let robotScore = 0;
+
+function robotPlay() {
+  let robotsHand = Math.floor(Math.random()*3);
+  if (robotsHand == 0) {
+    return "Rock";
+  } else if (robotsHand == 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+function humanPlay() {
+  humanHand = prompt("Please input either 'r' for rock, 'p' for paper, or 's' for scissors.");
+  if (humanHand == 'r') {
+    return 'Rock';
+  } else if (humanHand == 'p') {
+    return 'Paper';
+  } else if (humanHand == 's') {
+    return 'Scissors';
+  } else {
+    humanPlay();
+  }
+}
+
+function compareHands() {
+  if (humanScore >= 5) {
+    humanScore = 0;
+    robotScore = 0;
+    currentlyPlaying = false;
+    return 'Supreme victory!'
+  }
+  if (robotScore >= 5) {
+    humanScore = 0;
+    robotScore = 0;
+    currentlyPlaying = false;
+    return 'Mortifying failure...'
+  }
+  robotsHand = robotPlay();
+  humanHand = humanPlay();
+  if (robotsHand == 'Rock') {
+    if (humanHand == 'Rock') {
+      return keepScore('Tie');
+    } else if (humanHand == 'Paper') {
+      return keepScore('You win!');
+    } else {
+      return keepScore('You lose... that\'s kinda cringe');
+    }
+  } else if (robotsHand == 'Paper') {
+    if (humanHand == 'Rock') {
+      return keepScore('You lose... that\'s kinda cringe');
+    } else if (humanHand == 'Paper') {
+      return keepScore('Tie');
+    } else {
+      return keepScore('You win!');
+    }
+  } else {
+    if (humanHand == 'Rock') {
+      return keepScore('You win!');
+    } else if (humanHand == 'Paper') {
+      return keepScore('You lose... that\'s kinda cringe');
+    } else {
+      return keepScore('Tie');
+    }
+  }
+}
+
+function keepScore(string) {
+  if (string == 'You win!') {
+    humanScore++;
+    if (humanScore >= 5){
+      currentlyPlaying = false;
+      return 'Supreme victory!' + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+    }
+    return string + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+  } else if (string == 'You lose... that\'s kinda cringe') {
+    robotScore++;
+    if (robotScore >= 5){
+      currentlyPlaying = false;
+      return 'Mortifying failure!' + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+    }
+    return string + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+  } else if (string == 'Tie') {
+    return string + '\nHuman score: ' + humanScore + '\nrobotScore: ' + robotScore;
+  }
+}
+
+function playGame() {
+  do {
+    let result = compareHands();
+    console.log(result);
+  } while (currentlyPlaying == true)
+}
+
+playGame(); 
+
+*/
